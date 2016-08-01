6abb23ab42062055e2f1c4a81437818e027e1344
dchao <dchao@caltech.edu>
2016-08-01 09:01:25 -0700

    Updated proposed sideband plots.

--- data/download_generic.sql
+++ data/download_generic.sql
@@ -5,12 +5,18 @@ CREATE TEMPORARY VIEW meta AS
 SELECT * FROM 
 (ml_sample INNER JOIN event_weights_generic USING (eid))
 INNER JOIN 
-event_labels_generic USING (eid);
+event_labels_generic_augmented USING (eid);
 
 CREATE TEMPORARY VIEW events AS
 SELECT * FROM 
 (meta INNER JOIN candidate_optimized_events_generic USING (eid))
 INNER JOIN
-candidate_optimized_events_scores_generic USING (eid);
+(SELECT * 
+ FROM candidate_optimized_events_scores_generic_t 
+ WHERE (logit_logre_signal_score IS NOT NULL) AND
+       (logit_gbdt300_signal_score IS NOT NULL) AND
+       (logit_logre_dstartau_score IS NOT NULL) AND
+       (logit_gbdt300_dstartau_score IS NOT NULL)) AS Q
+USING (eid);
 
 \copy (SELECT * FROM events) TO 'event_features.csv' WITH CSV HEADER;
